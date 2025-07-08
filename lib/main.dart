1714e5ff857e520bd60c9166b47c0ba111bf3ba2
SebastianCiuiX2 <sebastian.ciui@x2mobile.net>
2025-07-08 14:42:50 +0300

    Create MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,35 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main(){
+void main() {
+  runApp(MyApp());
 }
 
-class MyAppState extends ChangeNotifier{
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: "Flutter App",
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+
+
+}
+
+class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
 }
 
-class MyHomePage extends StatelessWidget{
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch();
