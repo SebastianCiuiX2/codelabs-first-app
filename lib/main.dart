773eaf75d9b12fb09c98cf8bae0530ec1cb2cc6f
SebastianCiuiX2 <sebastian.ciui@x2mobile.net>
2025-07-08 14:38:37 +0300

    Create MyHomePage

--- lib/main.dart
+++ lib/main.dart
@@ -8,3 +8,19 @@ void main(){
 class MyAppState extends ChangeNotifier{
   var current = WordPair.random();
 }
+
+class MyHomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch();
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('A random idea'),
+          Text(appState.current.asLowerCase)
+        ],
+      ),
+    )
+  }
+
+}
